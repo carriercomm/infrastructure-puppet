dad9b00bb3492cfc5ef98cf080620a023bf3257a
Geoffrey Corey <coreyg@apache.org>
2015-04-28 12:42:16 -0700

    change to include instead of using class

--- modules/spamassassin/manifests/spamc.pp
+++ modules/spamassassin/manifests/spamc.pp
@@ -11,7 +11,7 @@ class spamassassin::spamc (
 
 ) {
 
-  class { 'haproxy': }
+  include haproxy
 
   class { "spamassassin::spamc::install::${::asfosname}::${::asfosrelease}":
     spamd_peers           => $spamd_peers,
